8a2b23ae8604ba22e03b6b6252a2addecec14e7b
Helen Shin <helen@helens-air.lan>
2021-04-23 14:28:18 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import BoxContainer from './BoxContainer.js'
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <BoxContainer />
+    </div>
+  );}
+}
+
+export default App;
